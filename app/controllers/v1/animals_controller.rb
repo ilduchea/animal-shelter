347fb3dd41a196efb83921957287239237cdd3b7
ilduchea <ilduchea@gmail.com>
2017-07-21 09:44:01 -0700

    add versioning. Passing.

---
+++ app/controllers/v1/animals_controller.rb
@@ -0,0 +1,3 @@
+class V1::AnimalsController < ApplicationController
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :animals
+  concern :api_base do
+    resources :animals
+  end
+  
+  namespace :v1 do
+    concerns :api_base
+  end
 end
