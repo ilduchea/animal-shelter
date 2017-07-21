8ec31c28b0e80349137157e154bced0545f2712f
ilduchea <ilduchea@gmail.com>
2017-07-21 10:04:50 -0700

    add tests for animal scopes. Passing.

--- spec/models/animal_spec.rb
+++ spec/models/animal_spec.rb
@@ -5,4 +5,40 @@ RSpec.describe Animal, type: :model do
   it { should validate_presence_of :species }
   it { should validate_presence_of :breed }
   it { should validate_presence_of :age }
+
+  describe "scopes", type: :request do
+    before do
+      5.times do
+        FactoryGirl.create(:animal)
+      end
+      animals = Animal.all
+      @animal1 = animals[0]
+      @animal2 = animals[1]
+    end
+
+    describe "name_search" do
+      it "returns all animals where search params equals the name" do
+        get "/v1/animals?name_search=#{@animal2.name}"
+        expect(JSON.parse(response.body).first['name']).to eq(@animal2.name)
+      end
+    end
+    describe "species_search" do
+      it "returns all animals where search params equals the species" do
+        get "/v1/animals?species_search=#{@animal2.species}"
+        expect(JSON.parse(response.body).first['species']).to eq(@animal2.species)
+      end
+    end
+    describe "breed_search" do
+      it "returns all animals where search params equals the breed" do
+        get "/v1/animals?breed_search=#{@animal2.breed}"
+        expect(JSON.parse(response.body).first['breed']).to eq(@animal2.breed)
+      end
+    end
+    describe "age_search" do
+      it "returns all animals where search params equals the age" do
+        get "/v1/animals?age_search=#{@animal2.age}"
+        expect(JSON.parse(response.body).first['age']).to eq(@animal2.age)
+      end
+    end
+  end
 end
