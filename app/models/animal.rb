236599d6fd3dba9832b3943268f5adcd57f756b2
ilduchea <ilduchea@gmail.com>
2017-07-21 09:32:22 -0700

    add base scopes for animals, with base routes.

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,5 +1,8 @@
 class Animal < ApplicationRecord
   validates :name, :species, :breed, :age, presence: true
 
-  
+  scope :name_search, -> (name){ where("lower(name) like ?", "%#{name}%".downcase) }
+  scope :species_search, -> (species){ where("lower(species) like ?", "%#{species}%".downcase) }
+  scope :breed_search, -> (breed){ where("lower(breed) like ?", "%#{breed}%".downcase) }
+  scope :age_search, -> (age){ where(age: age) }
 end
